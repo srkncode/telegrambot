77db1db7a5669df49f60d9b9d4f502d9c7fc92dc
srkncode <srknmetro@gmail.com>
2025-04-18 08:46:54 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,27 +1,25 @@
-import json
 import os
 import telegram
+import json
+from fastapi import FastAPI, Request
 
 BOT_TOKEN = os.environ.get("BOT_TOKEN")
 bot = telegram.Bot(token=BOT_TOKEN)
 
-def telegram_webhook(request):
-    if request.method == 'POST':
-        try:
-            update = telegram.Update.de_json(json.loads(request.data.decode('utf-8')), bot)
-            if update.message:
-                chat_id = update.message.chat_id
-                text = update.message.text
+app = FastAPI()
 
-                if text == "/start" or text == "/merhaba":
-                    bot.send_message(chat_id=chat_id, text="Merhaba! Ben serverless bir Telegram botuyum.")
-                elif text == "/yardim":
-                    bot.send_message(chat_id=chat_id, text="Şu an sadece /merhaba ve /yardim komutlarını destekliyorum.")
-                else:
-                    bot.send_message(chat_id=chat_id, text="Anlamadım.")
+@app.post("/webhook")
+async def webhook(request: Request):
+    data = await request.json()
+    update = telegram.Update.de_json(data, bot)
+    if update.message:
+        chat_id = update.message.chat_id
+        text = update.message.text
 
-            return "OK", 200
-        except Exception as e:
-            print(f"Hata oluştu: {e}")
-            return "Hata", 500
-    return "OK", 200
+        if text == "/start" or text == "/merhaba":
+            bot.send_message(chat_id=chat_id, text="Merhaba! Ben serverless bir Telegram botuyum.")
+        elif text == "/yardim":
+            bot.send_message(chat_id=chat_id, text="Şu an sadece /merhaba ve /yardim komutlarını destekliyorum.")
+        else:
+            bot.send_message(chat_id=chat_id, text="Anlamadım.")
+    return "OK"
